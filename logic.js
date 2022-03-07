55ea9c28a7204da198062abb31ca52222e3dda8e
Erikka Amrine <erikkaamrine@hotmail.com>
2022-03-06 19:24:07 -0500

    basic mapping

---
+++ logic.js
@@ -0,0 +1,8 @@
+//create a timed quiz
+//display the first question
+//call upon an array for questions and answers
+//series of prompts
+//keep score
+//when question is incorrect or time runs out the game ends
+//score is displayed
+//score is kept in local storage to show top 5 scorers
