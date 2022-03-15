bf322953816b64a7144bf5044f27f812ba98cbad
Erikka Amrine <erikkaamrine@hotmail.com>
2022-03-15 17:21:57 -0400

    landing page setup

--- logic.js
+++ logic.js
@@ -6,3 +6,16 @@
 //when question is incorrect or time runs out the game ends
 //score is displayed
 //score is kept in local storage to show top 5 scorers
+function finalScore()
+
+var highscore = localStorage.setItem(finalScore);
+
+var highscore = localStorage.getItem('highscore');
+if (highscore === null){
+    highscore = 0;
+}
+
+//filter index to 4 in order to get top 5 
+//getResultById()
+
+//
